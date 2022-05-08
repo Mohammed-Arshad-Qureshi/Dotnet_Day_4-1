0c635d7bc5c31b5b6a6894cc400ea35ecba5a69e
Arshad <mohammedarshad183@gmail.com>
2022-05-08 20:09:48 +0530

    New: Creating Snake and Ladders game

---
+++ SnakeLadders/SnakeLadders/SnakeLadder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeLadders
+{
+    class SnakeLadder
+    {
+        public void Play()
+        {
+            Console.WriteLine("Ready to Play Snake and Ladders");
+            Console.ReadLine();
+        }
+    }
+}
